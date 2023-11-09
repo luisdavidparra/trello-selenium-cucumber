9f0132b869704f2cc09773faf2235d4b314ac764
Luis David <luisdavidparrah@gmail.com>
2023-11-08 20:10:46 -0400

    add cucumber world with setDefaultTimeout

---
+++ test/support/step_definitions/world.js
@@ -0,0 +1,12 @@
+const { setWorldConstructor, setDefaultTimeout, World } = require("@cucumber/cucumber");
+const { cucumberTimeout } = require("../../../env.json");
+
+setDefaultTimeout(cucumberTimeout);
+
+class CustomWorld extends World {
+  constructor(options) {
+    super(options);
+  }
+}
+
+setWorldConstructor(CustomWorld);
